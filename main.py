5f0225c42809e20f58b0bc4d59235aa9392dc7f8
binary <romaingoncalves25@gmail.com>
2017-07-13 22:16:44 +0200

    System call with -r and -c options

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import func_update
 
 # Var.
 proxy_download = "data/index"
-proxy_file = "data/list"
+proxy_list = "data/list"
 proxy_conf = "data/proxychains.conf"
 proxy_template = "data/proxychains.template"
 
@@ -20,18 +20,26 @@ if len(sys.argv) > 1:
 	# Manual Proxy
 	if sys.argv[1] == "-c" or sys.argv[1] == "--choose":
 		func_proxy.manual(proxy_conf, proxy_template)
-
+		
+		# Exec program
+		if len(sys.argv) >= 6:
+			func_user.exec("/usr/bin", sys.argv[5], 6)
+		
 	# Random Proxy
 	elif sys.argv[1] == "-r" or sys.argv[1] == "--random":
-		func_proxy.random(proxy_file, proxy_conf, proxy_template)
+		func_proxy.random(proxy_list, proxy_conf, proxy_template)
+		
+		# Exec program
+		if len(sys.argv) >= 3:
+			func_user.exec("/usr/bin", sys.argv[2], 3)
 
 	# Update Proxy list
 	elif sys.argv[1] == "-u" or sys.argv[1] == "--update":
-		func_update.update(proxy_download, proxy_file)
+		func_update.update(proxy_download, proxy_list)
 
 	# Show Available Proxy
 	elif sys.argv[1] == "-l" or sys.argv[1] == "--list":
-		func_proxy.list()
+		func_proxy.list(proxy_list)
 
 	# Display Help
 	elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
